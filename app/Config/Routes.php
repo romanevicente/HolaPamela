5c0926a9697caceccfe2d7635f9ff7dbbd64b02b
Ines Sakout <ines@hello-pomelo.com>
2023-05-11 18:05:51 +0200

    route index

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -30,6 +30,8 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+//$routes->get('/index', 'RecommendationsController::index');
+$routes->get('/recommendations(:num)', 'RecommendationsController::recommendations/$1');
 
 /*
  * --------------------------------------------------------------------
---
+++ app/Views/pages/index.php
@@ -0,0 +1,1 @@
+<h1>page d'accueil</h1>
