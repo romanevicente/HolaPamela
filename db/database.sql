9b50fbc558b7c411f0cd2e879b1ed602f6031816
La-CS <contact@lacodingschool.com>
2023-05-16 15:36:04 +0200

    Add db inserts

---
+++ db/database.sql
@@ -0,0 +1,17 @@
+INSERT INTO categories (title) VALUES ("Restaurants");
+INSERT INTO categories (title) VALUES ("Bars");
+INSERT INTO categories (title) VALUES ("Plages");
+INSERT INTO categories (title) VALUES ("Randonnées");
+INSERT INTO categories (title) VALUES ("Hotels");
+INSERT INTO categories (title) VALUES ("Musées");
+INSERT INTO categories (title) VALUES ("Parcs");
+
+
+
+INSERT INTO recommendations (title, address, description, author, picture, category_id)
+VALUES ("La candela", "Plaça de Sant Pere, 12", "A compléter", "Ines", "pictures/placeholder.png", 1);
+
+INSERT INTO recommendations (title, address, description, author, picture, category_id)
+VALUES ();
+
+
