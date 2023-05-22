266e67dcdcd36d6693f63960892236986de263ce
Romane Vicente <romane@hello-pomelo.com>
2023-05-22 23:25:58 +0200

    correction form

--- app/Views/pages/form.php
+++ app/Views/pages/form.php
@@ -1,54 +1,75 @@
 <!DOCTYPE html>
+<html>
+<head>
+  <title>Partage une nouvelle recommandation</title>
+</head>
+<body>
+    
+    <h2>Partage une nouvelle recommandation</h2>
+    
+    <div class="container pt-5">
+  <div class="row justify-content-start">
+    <div class="col-12 col-md-6 col-lg-4">
+        <form method="post" action="/form">
+        <div class="row mb-3">
+            <label for="title" class="col-sm-2 col-form-label">Titre</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" name="title" required>
+            </div>
+        </div>
 
-<h2>Partage une nouvelle recommandation</h2>
+        <div class="row mb-3">
+          <label for="categorie" class="col-sm-2 col-form-label">Catégorie</label>
+          <div class="col-sm-10">
+              <select class="form-select" name="categorie">
+                  <option selected>Choisissez...</option>
+                  <?php foreach($categoriesList as $categorie): ?>
+                <option value="7"><?=$categorie["title"]?></option>
+              <?php endforeach ?>
+            </select>
+          </div>
+        </div>
 
-<form method="post" action="/form">
+        <div class="row mb-3">
+          <label for="description" class="col-sm-2 col-form-label">Description</label>
+          <div class="col-sm-10">
+              <textarea class="form-control" name="description" rows="3" required></textarea>
+            </div>
+        </div>
+        
+        <div class="row mb-3">
+            <label for="address" class="col-sm-2 col-form-label">Adresse</label>
+            <div class="col-sm-10">
+            <input type="text" class="form-control" name="address" required>
+        </div>
+    </div>
+    
     <div class="row mb-3">
-    <label for="titLe" class="col-sm-1 col-form-label">Titre</label>
-    <div class="col-sm-10">
-    <input type="text" class="form-field" name="title" required>
+        <label for="author" class="col-sm-2 col-form-label">Auteur</label>
+        <div class="col-sm-10">
+            <input type="text" class="form-control" name="author" required>
+        </div>
+    </div>
+    
+        <div class="row mb-3">
+            <label for="picture" class="col-sm-2 col-form-label">Photo</label>
+          <div class="col-sm-10">
+              <input type="file" class="form-control" name="picture" required>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-10 offset-sm-2">
+                <button type="submit" class="btn btn-primary">Je partage</button>
+            </div>
+        </div>
+    </form>
     </div>
 </div>
-
-<div class="input-group mb-3">
-<label for="categorie" class="col-sm-1 col-form-label">Catégorie</label>
-  <select class="form-select">
-  <option selected>Choisissez...</option>
-        <?php foreach($categoriesList as $categorie): ?>
-            <option value="7"><?=$categorie["title"]?></option>
-        <?php endforeach ?>
-
-  </select>
 </div>
 
-<div class="row mb-3">
-    <label for="description" class="col-sm-1 col-form-label">Description</label>
-    <div class="col-sm-10">
-    <input type="text" class="form-field" name="description" required>
-    </div>
-</div>
-
-<div class="row mb-3">
-    <label for="address" class="col-sm-1 col-form-label">Adresse</label>
-    <div class="col-sm-10">
-    <input type="text" class="form-field" name="address" required>
-    </div>
-</div>
-
-<div class="row mb-3">
-    <label for="author" class="col-sm-1 col-form-label">Auteur</label>
-    <div class="col-sm-10">
-    <input type="text" class="form-field" name="author" required>
-    </div>
-</div>
-
-<div class="mb-3">
-  <label for="picture" class="col-sm-1 col-form-label">Photo</label>
-  <input class="form-field" type="file" name="picture" required>
-</div>
-
-<button type="submit" class="btn btn-primary">Je partage</button>
-
-</form>
 </body>
 </html>
+
+
+<!-- 
