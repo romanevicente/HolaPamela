e1fab15190df63ace4b77d590aa072c53a445485
Ines Sakout <ines@hello-pomelo.com>
2023-05-23 16:20:05 +0200

    grid homepage

--- app/Views/pages/index.php
+++ app/Views/pages/index.php
@@ -1,10 +1,19 @@
-<h1>page d'accueil</h1>
-
-
-<?php foreach($recommendationsList as $recommendation): ?>
-              <p class="nav-link" href="#"><?=$recommendation["title"]?></p>
-              <p class="nav-link" href="#"><?=$recommendation["address"]?></p>
-              <p class="nav-link" href="#"><?=$recommendation["description"]?></p>
-              <p class="nav-link" href="#"><?=$recommendation["author"]?></p>
-              <img src="<?=$recommendation["picture"]?>">     
-<?php endforeach ?>
+    <!-- <div class="cards-container"> -->
+      <!-- Cards -->
+      <div class="">
+        <div class="container">
+            <div class="row row-cols-4">
+                <?php foreach($recommendationsList as $recommendation): ?>
+                    <div class="col"> 
+                    <p class="nav-link recommendation-title" href="#"><?=$recommendation["title"]?></p>
+                    <p class="nav-link" href="#"><?=$recommendation["address"]?></p>
+                    <p class="nav-link" href="#"><?=$recommendation["description"]?></p>
+                    <p class="nav-link" href="#"><?=$recommendation["author"]?></p>
+                    <img src="<?=$recommendation["picture"]?>">  
+                </div>
+   
+                <?php endforeach ?></div>
+            </div>
+        </div>
+      </div>
+    </div>
