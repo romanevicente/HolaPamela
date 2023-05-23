4d89a34b7d5fdd4eed43b81847f638635b845363
Ines Sakout <ines@hello-pomelo.com>
2023-05-23 16:56:05 +0200

    front homepage

--- app/Views/pages/index.php
+++ app/Views/pages/index.php
@@ -1,15 +1,22 @@
+    
     <!-- <div class="cards-container"> -->
       <!-- Cards -->
       <div class="">
+      
         <div class="container">
+        <h3>Vous pourriez aimer...</h3>
             <div class="row row-cols-4">
                 <?php foreach($recommendationsList as $recommendation): ?>
                     <div class="col"> 
-                    <p class="nav-link recommendation-title" href="#"><?=$recommendation["title"]?></p>
-                    <p class="nav-link" href="#"><?=$recommendation["address"]?></p>
-                    <p class="nav-link" href="#"><?=$recommendation["description"]?></p>
-                    <p class="nav-link" href="#"><?=$recommendation["author"]?></p>
-                    <img src="<?=$recommendation["picture"]?>">  
+                    <img class="img" src="<?=$recommendation["picture"]?>">  
+                    <span class="recommendation-title" href="#"><?=$recommendation["title"]?></span>
+                    <br>
+                    <span class="" href="#"><?=$recommendation["address"]?></span>
+                    <br>
+                    <span class="" href="#"><?=$recommendation["description"]?></span>
+                    <br>
+                    <span class="" href="#"><?=$recommendation["author"]?></span>
+                    <br>
                 </div>
    
                 <?php endforeach ?></div>
