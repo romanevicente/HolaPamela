75729fb67027de16745e4a252f3d5d016b8182e2
Romane Vicente <romane@hello-pomelo.com>
2023-05-25 17:05:15 +0200

    cat

--- app/Views/pages/cat.php
+++ app/Views/pages/cat.php
@@ -1,11 +1,17 @@
-<?php foreach($recommandationList as $recommendation): ?>
-              <p class="nav-link" href="#"><?=$recommendation["title"]?></p>
-              <p class="nav-link" href="#"><?=$recommendation["address"]?></p>
-              <p class="nav-link" href="#"><?=$recommendation["description"]?></p>
-              <p class="nav-link" href="#"><?=$recommendation["author"]?></p>
-              <img src="<?=$recommendation["picture"]?>">     
-
-              <a href="/recommendations/<?=$recommendation["id"]?>" class="button">Show more</a>
-
-<?php endforeach ?>
-
+<div class="container">
+<div class="col row row-cols-4">
+  <?php foreach($recommandationList as $recommendation): ?>
+    <div class="col">
+    <img class="img" src="<?=$recommendation["picture"]?>">  
+    <span class="recommendation-title" href="#"><?=$recommendation["title"]?></span>
+  <br>
+      <span class="nav-link" href="#"><?=$recommendation["address"]?></span>
+      <span class="nav-link" href="#"><?=$recommendation["description"]?></span>
+      <span class="nav-link" href="#"><?=$recommendation["author"]?></span>
+      <img src="<?=$recommendation["picture"]?>">     
+      <a href="/recommendations/<?=$recommendation["id"]?>" class="button">Voir plus</a>
+      <?php endforeach ?>
+</div>
+  </div>
+  </div>
+  
