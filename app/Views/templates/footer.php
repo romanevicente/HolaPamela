34ed5e485063ddbc40c9934884509ef27bc3bc0a
Marion Magaud <marion@hello-pomelo.com>
2023-05-30 17:37:46 +0200

    ajoutfooter

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -2,16 +2,16 @@
 <footer class="footer">
 
   <div style="text-align: left;">
-    <h3>Hola Pamela</h3>
+        <span class="title-footer">Hola Pamela</span>
+        <img class="logo" src="/pictures/testlogo.png" alt="Logo">
   </div>
+
+  <div style="text-align: left;">
+        </div>
     <div style="text-align: center;">
         <p>Merci à tous!</p>
         <p>© Romane, Ines et Marion</p>
         <p>Juin 2023</p>
-
-        <div style="text-align: left;">
-            <img src="public/pictures/testlogo.png" alt="Logo" width="100">
-        </div>
     </div>
   
 
